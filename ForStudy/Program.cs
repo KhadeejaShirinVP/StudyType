4c5f2b9d2d4d7efae089569a636e51869f6f503f
Khadeeja-Shirin <shirinalnajm@gmail.com>
2022-09-27 23:26:09 +0530

    roughNotes

---
+++ ForStudy/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForStudy
+{
+    internal class Program
+    {
+        //**types of variable.
+        static int e=33; //*Static Variable
+        int f=4567; //*Non-Static or Instance Variable
+        //static void Main(string[] args)
+        //{
+        //    int z; //*Static Variable
+        //}
+        //*//Constant Variable
+        const float PI = 3.14f;
+
+        //*//Readonly Variable   
+        readonly int z; 
+        //Constructor
+        public Program(int a)
+        {
+            //Initializing non-static variable
+            f = a;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a decimal value");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            // Explicit type casting
+            int num2 = (int)num1;
+            Console.WriteLine("original double value : "+ num1);
+            Console.WriteLine("Converted int value : "+ num2);
+            Console.ReadKey();
+
+            int IntNum1 = 450;
+            byte ByteNum1 = (byte)IntNum1; // Explicit Type Casting
+            // Printing the Original Value and Converted Value
+            Console.WriteLine($"Original Value:{IntNum1} \nand Converted Value:{ByteNum1}");
+            Console.ReadKey();
+
+            //Converting string to Integer
+            string word = "100";
+            int num =Convert.ToInt32(word);
+            Console.ReadKey();
+
+            string word1 = "198";
+            //Converting string to Integer
+            int number = Convert.ToInt32(word1);  
+            double d = 567.98;
+            //Converting double to Integer
+            int number1 = Convert.ToInt32(d);  
+            float f = 23.516F;
+            //Converting float to string
+            string word2 = Convert.ToString(f);
+
+            //**Arithemetic Operators
+            int Rst;
+            int Num4=9, Num5=2;
+            // additional operator
+            Rst = (Num4 + Num5);
+            //subtraction operator
+            Rst = (Num4 - Num5);
+            //multiplication operator
+            Rst = (Num4 * Num5);
+            //divion operator
+            Rst = (Num4 / Num5);
+            // Modulo Operation
+            Rst = (Num4 % Num5);
+
+            //**Assignment Operators
+            // Initialize variable x using Simple Assignment Operator "="
+            int x = 20;
+            x += 10;  //It means x = x + 10 i.e. 20 + 10 = 30
+            x -= 5;  //It means x = x - 5 i.e. 20 - 5 = 15
+            x *= 5; //It means x = x * 5  i.e. 20 * 5 = 100
+            x /= 5; //It means x = x / 5 i.e. 20 / 5 = 4
+            x %= 5; //It means x = x % 5 i.e. 20 % 5 = 0
+
+
+            //**Relational Operators.
+            bool rst;
+            int k = 5, y = 10;
+            // Equal to Operator
+            rst = (k == y);
+            // Greater than Operator
+            rst = (k > y);
+            // Less than Operator
+            rst = (k < y);          
+            // Greater than Equal to Operator
+            rst = (k >= y);          
+            // Less than Equal to Operator
+            rst = (k <= y);           
+            // Not Equal To Operator
+            rst = (k != y);
+
+
+            bool m = true, n = false, o;
+            //Logical AND operator
+            o = m && n;
+            //Logical OR operator
+            o = m || n;
+            //Logical NOT operator
+            o = !m;
+
+
+            //**beark
+            for(int i = 0; i <= 10; i++)
+            {
+                if(i == 5)
+                    break;
+                Console.WriteLine(i);
+            }Console.ReadKey();
+
+
+            //**continue
+            for(int s = 0; s <= 10; s++)
+            {
+                if (s == 5)
+                    continue;
+                Console.WriteLine(s);
+            }Console.ReadKey();
+
+
+
+            //**go to
+            int p = 2;
+        EVEN:
+            Console.Write($"{p} ");
+            p += 2;
+            if (p <= 30)
+                goto EVEN;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ forStudy2/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //writeLine Method Print the value and move the ursor to the next line.
+            Console.WriteLine("Hello");
+            // Write Method Print the value and estay in the same line.
+            Console.Write("Greeting a Day ");
+            Console.WriteLine("Divya");
+            Console.WriteLine("Bye");
+            //Readkey is used to read a single character from the keyboard, and return that character.
+            Console.ReadKey();
+
+
+            string place = "kerala";
+            Console.WriteLine(place);
+            Console.WriteLine("Welcome to " + place);
+            Console.ReadKey();
+
+
+            //Giving one message to the user
+            Console.WriteLine("Enter your Pet name");
+            //ReadLine method reads a string value from the keyboard.
+            string pet = Console.ReadLine();
+            //printing the entered string in the console.
+            Console.WriteLine($"So cute {pet}");
+            Console.ReadKey();
+
+
+            //giving a message to user for enter two numbers.
+            Console.WriteLine("Enetr two Numbers");
+            //converting first string to integer
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //converting second string to integer
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            // the result is Assigned in sum.
+            int sum = num1 + num2;
+            // 3 types of reult.
+            Console.WriteLine("The Sum is: " + sum);
+            Console.WriteLine($"The Sum is: {sum}");
+            Console.WriteLine($"The Sum is: {num1 + num2}");
+            Console.ReadKey();
+
+            //*properties of console*//
+            //to sets backgroundColour
+            Console.BackgroundColor = ConsoleColor.Black;
+            //to sets Foreground Colour
+            Console.ForegroundColor = ConsoleColor.Red;
+            //to sets the title to diplay in the console.
+            Console.Title = "Methods of Console";
+            Console.WriteLine("Title: Methods of Console");
+            Console.WriteLine("BackgroundColor: Black");
+            Console.WriteLine("ForegroundColor: Red");
+            //It will make Beep Sound
+            Console.Beep();
+            Console.ReadKey();
+
+
+            //**if else
+            int number1;
+            //giving a message to user for enter a number
+            Console.WriteLine("Enter a Number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            if (number1 > 15)
+                Console.WriteLine($"{number1} is greater than 15 ");  //This Statement belongs to IF Block
+            else
+                Console.WriteLine($"{number1} is less than 15");   //This Statement belongs to ELSE Block
+            Console.ReadKey();
+
+
+            //**ladder if-else
+            int b = 40;
+            if (b == 10)
+                Console.WriteLine("b is 10");
+            else if (b == 15)
+                Console.WriteLine("b is 15");
+            else if (b == 20)
+                Console.WriteLine("b is 40");
+            else
+                Console.WriteLine("b is not available");
+            Console.ReadKey();
+
+            //**switch
+            string str = "C#";
+            Console.WriteLine("Control Flow Statements");
+            Console.WriteLine("enter any control flow statement");
+            str = Console.ReadLine();
+            switch (str)
+            {
+                case "if":
+                case "if-else":
+                case "switch":
+                    Console.WriteLine("It's a selection statements");
+                    break;
+                case "for":
+                case "while":
+                case "do-whie":
+                    Console.WriteLine("It's a iteration statements");
+                    break;
+                case "goto":
+                case "break":
+                case "continue":
+                    Console.WriteLine("It's a jump statements");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+            Console.ReadKey();
+
+
+            //*while loop
+            int j = 0;
+            while (j <= 5)
+            {
+                Console.WriteLine("Value of j:" + j);
+                j++;
+            }
+            Console.ReadKey();
+
+            //**do while
+            int a = 1;
+            do
+            {
+                Console.Write($"{a} ");
+                a++;
+            } while (a <= 5);
+            Console.ReadKey();
+
+
+            //**for loop
+            for (int m = 1; m <= 5; m++) //Outer For Loop
+            {
+                Console.WriteLine($"numbers are : {m}");
+                for (int n = 1; n <= 10; n++) //Inner For Loop
+                Console.Write($"{n}");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
