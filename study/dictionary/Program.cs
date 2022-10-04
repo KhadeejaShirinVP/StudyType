5e86021ae7463e3b5fa7adc3077e7e0e62789290
Khadeeja-Shirin <shirinalnajm@gmail.com>
2022-10-04 23:30:40 +0530

    study

---
+++ study/dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Dictionary<string, long> PhoneBook = new Dictionary<string, long>();
+            //PhoneBook.Add("shirin", 7025461009); //PhoneBook["shirin"] = 7025461009;
+            //PhoneBook.Add("afsal", 9744495424); //PhoneBook["afsal"] = 9744495423;
+            //PhoneBook.Add("mummy", 8086609498);
+
+            //if (PhoneBook.ContainsKey("mummy"))    //for checking. phonbook il shirin ndo enn nokan.
+            //{
+            //    Console.WriteLine("mummy number is " + PhoneBook["mummy"]);
+            //}
+            //Console.ReadKey();
+
+            string name;
+            Dictionary<string, long> PhoneBook = new Dictionary<string, long>();
+            Console.WriteLine("enter your choose name");
+            name = Console.ReadLine();
+            PhoneBook.Add("shirin", 7025461009); //PhoneBook["shirin"] = 7025461009;
+            PhoneBook.Add("afsal", 9744495424); //PhoneBook["afsal"] = 9744495423;
+            PhoneBook.Add("mummy", 8086609498);
+
+            if (PhoneBook.ContainsKey(name))    //for checking. phonbook il shirin ndo enn nokan.
+            {
+                Console.WriteLine("number is " + PhoneBook[name]);
+            }
+            //Console.WriteLine(PhoneBook.Count); //for length of phnbook
+            Console.ReadKey();
+
+          //Dictionary<string, long> PhoneBook2 = new Dictionary<string, long>();
+          //DataStructure<key, value> variable= new variable<key,value>();
+          //*key is unique,not allow to null
+          //*value allow to null(if enable null)
+        }
+    }        
+}
---
+++ study/study/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers=new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+
+            //numbers.Remove(2);-- for remove,here 2 is value.
+           // Console.WriteLine(numbers.ElementAt(1));--here 1 is index
+
+
+            //List<int> numbers2=new List<int>();
+            //numbers2.Add(4);
+            //numbers2.Add(5);
+            //numbers2.Add(6);
+            //numbers.AddRange(numbers2); //for concatinaion
+            //Console.WriteLine(numbers.Count);
+            //Console.ReadKey();
+
+            //List<int> numbers = new List<int>();
+            //numbers.Add(1);
+            //numbers.Add(2);
+            //numbers.Add(3);
+
+            int[] array = new int[] { 10, 20, 30 };
+            numbers.AddRange(array);   // concatinating array
+            //Console.WriteLine(numbers.Count);
+            //Console.ReadKey();
+            Console.WriteLine(numbers.ElementAt(3));
+            Console.ReadKey();
+
+           
+        }
+    }
+}
